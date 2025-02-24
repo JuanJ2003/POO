7d7aa138bb4c23c4dafba22ccec905310da573de
JuanJJ <juanjacanamejoy03@gmail.com>
2025-02-24 11:25:06 -0500

    archivo persona V1

--- Persona.py
+++ Persona.py
@@ -1,2 +1,29 @@
 class persona:
-    pass
+    def __init__(self,nombre,direccion,edad):
+        self.__nombre=nombre
+        self.__direccion=direccion
+        self.__edad=edad
+      
+    def get_nombre(self):
+        return self.__nombre
+    
+    def set_nombre(self,nombre):
+        self.__nombre= nombre
+        
+    def get_edad(self):
+        return self.__edad
+    
+    def set_edad(self,edad):
+        self.__edad = edad
+        
+    def get_direccion(self):
+        return self.__direccion
+    
+    def set_direccion(self,direccion):
+        self.__direccion = direccion
+        
+    def __str__(self):
+        print("la lista de estudiantes es: ", self.__nombre)
+    
+ 
+    
